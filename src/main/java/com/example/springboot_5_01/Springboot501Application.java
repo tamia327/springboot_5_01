5a7d2d4c802acab6650118b77a41349c8d328de7
tamiawhite <tamia327@gmail.com>
2018-12-07 08:33:32 -0500

    Initial commit

---
+++ src/main/java/com/example/springboot_5_01/Springboot501Application.java
@@ -0,0 +1,12 @@
+package com.example.springboot_5_01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Springboot501Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Springboot501Application.class, args);
+	}
+}
